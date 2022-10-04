1422c12f67c8197212613ed28b57224002f26748
Dmirty <9048881951@mail.ru>
2022-10-04 10:23:49 +0500

    +4.3

--- task_4_3.py
+++ task_4_3.py
@@ -19,3 +19,24 @@
 # out
 # [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
 # [6, 2, 3, 0, 9]
+import random
+
+
+def cr_nln():
+    while True:
+        try:
+            num = int(input('Введите размерность: '))
+            if num > 0:
+                list_num = [random.randint(-5, 5) for i in range(num)]
+                return list_num
+            return "Negative value of the number of numbers!"
+        except ValueError:
+            print('Это не число! Повторим: ')
+
+
+list_num = (cr_nln())
+list_out = []
+for i in range(len(list_num)):
+    if list_num.count(list_num[i]) == 1:
+        list_out.append(list_num[i])
+print(f'Исходные значения: {list_num}\nСписок неповторяющихся элементов: {list_out}')
