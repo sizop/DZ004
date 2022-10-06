6cdcdf896c6facba6a1477ebf47849928e1ebd5d
Dmirty <9048881951@mail.ru>
2022-10-06 06:51:03 +0500

    +4.2

--- task_4_1.py
+++ task_4_1.py
@@ -18,8 +18,17 @@
 # getcontext().prec = 3
 # print(Decimal(9**0.3))
 
-from decimal import *
-# d = int(input())
+from decimal import Decimal
+# d = float(input())
 # getcontext().prec = 10
-# print((Decimal(9))/Decimal(1))
-# # print(Decimal(9).quantize(Decimal(10)))
+# print(1*(Decimal(d)))
+# print(Decimal(9).quantize(Decimal(10)))
+p = input()
+raz=str(input('Вводим разрядность:'))
+raz=raz[::-1][:-2]
+raz.replace('1', '1.')
+# raz=raz[:-2]
+print(raz)
+a = Decimal(p)
+a = a.quantize(Decimal('1.0000'))#raz))
+print(a)
--- task_4_4.py
+++ task_4_4.py
@@ -20,3 +20,31 @@
 # 4*x^5 + 1*x^4 - 3*x^3 - 3 = 0
 # 4*x^2 - 4 = 0
 # 2*x^4 - 3*x^3 + 3*x^2 + 1*x^1 - 2 = 0
+from random import randint
+
+signs = ['+', '-']
+
+with open("out.txt", "a+", encoding="utf-8") as my_f:
+    for j in range(0, 3):
+        formula = []
+        num = int(input('Введите число:'))
+        if num == 0:
+            continue
+        i = 9
+        while i > 1:
+            if i == 7:
+                i-=1
+                continue
+            formula += (randint(1, num)), ('*x^'), (i)
+            i -= 1
+            if i == 1:
+                continue
+            formula += (' ', signs[randint(0, 1)], ' ')
+        formula += (' = 0')
+        j += 1
+
+        convert = ''.join(map(str, formula))
+        my_f.write(f'{convert}\n')
+        print(f'{convert}\n')
+
+
